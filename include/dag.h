b98957b1c1dde265af0accd02321f066a7c71901
stto <sspjyins@snu.ac.kr>
2021-05-28 21:46:20 +0900

    add dag.h

---
+++ include/dag.h
@@ -0,0 +1,31 @@
+//
+// Created by 심영인 on 2021/05/28.
+//
+
+#ifndef SUBGRAPH_MATCHING_DAG_H
+#define SUBGRAPH_MATCHING_DAG_H
+
+#include "common.h"
+#include "graph.h"
+#include "candidate_set.h"
+
+class Dag {
+public:
+    explicit Dag(const Graph& query, const CandidateSet& cs);
+    ~Dag();
+
+    inline int32_t GetGraphID() const;
+
+    inline size_t GetNumVertices() const;
+    inline size_t GetNumEdges() const;
+    inline size_t GetNumLabels() const;
+
+private:
+    int32_t graph_id_;
+
+    size_t num_vertices_;
+    size_t num_edges_;
+    size_t num_labels_;
+};
+
+#endif //SUBGRAPH_MATCHING_DAG_H
