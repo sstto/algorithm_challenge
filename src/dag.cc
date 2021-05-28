4953c291fb1b2f4b6a173962ea6b21e5e576d5be
stto <sspjyins@snu.ac.kr>
2021-05-28 21:43:17 +0900

    dag class : find a root of dag

--- src/dag.cc
+++ src/dag.cc
@@ -2,11 +2,23 @@
 
 
 
-Dag::Dag(const Graph &query) {
+Dag::Dag(const Graph &query, const CandidateSet &cs) {
     graph_id_ = query.GetGraphID();
     num_vertices_ = query.GetNumVertices();
     num_edges_ = query.GetNumEdges();
     num_labels_ = query.GetNumLabels();
+
+// find a root vertex using argmin of |C|/|Degree|
+    size_t root_vertex = 0;
+    size_t factor = cs.GetCandidateSize(0)/query.GetDegree(0);
+    for (size_t i = 0; i < num_vertices_; ++i) {
+        if (factor > cs.GetCandidateSize(i)/query.GetDegree(i)){
+            root_vertex = i;
+        }
+    }
+
+
+
 }
 
 int32_t Dag::GetGraphID() const {
