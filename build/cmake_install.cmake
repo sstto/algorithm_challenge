3aa62ff72085721c1e2527eb5eb6bf3cca2a76fb
stto <sspjyins@snu.ac.kr>
2021-06-06 13:17:36 +0900

    strange code

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-# Install script for directory: C:/Users/kyi88/git/algo_hw2
-=======
-# Install script for directory: /Users/sim-yeong-in/Google 드라이브/2021-1/알고리즘/hw2
->>>>>>> 7bf45b749dc989912942e89677dc52d472b77cbe
+# Install script for directory: /Users/sim-yeong-in/Google 드라이브/2021-1/알고리즘/hw2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/subgraph_matching")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -16,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -36,13 +32,14 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
+endif()
+
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-<<<<<<< HEAD
-  include("C:/Users/kyi88/git/algo_hw2/build/main/cmake_install.cmake")
-=======
-  include("/Users/sim-yeong-in/Google 드라이브/2021-1/알고리즘/hw2/build/main/cmake_install.cmake")
->>>>>>> 7bf45b749dc989912942e89677dc52d472b77cbe
+  include("/Users/sim-yeong-in/Google 드라이브/2021-1/알고리즘/hw2/build/main/cmake_install.cmake")
 
 endif()
 
@@ -54,9 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-<<<<<<< HEAD
-file(WRITE "C:/Users/kyi88/git/algo_hw2/build/${CMAKE_INSTALL_MANIFEST}"
-=======
-file(WRITE "/Users/sim-yeong-in/Google 드라이브/2021-1/알고리즘/hw2/build/${CMAKE_INSTALL_MANIFEST}"
->>>>>>> 7bf45b749dc989912942e89677dc52d472b77cbe
+file(WRITE "/Users/sim-yeong-in/Google 드라이브/2021-1/알고리즘/hw2/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- build/main/cmake_install.cmake
+++ build/main/cmake_install.cmake
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-# Install script for directory: C:/Users/kyi88/git/algo_hw2/main
-=======
-# Install script for directory: /Users/sim-yeong-in/Google 드라이브/2021-1/알고리즘/hw2/main
->>>>>>> 7bf45b749dc989912942e89677dc52d472b77cbe
+# Install script for directory: /Users/sim-yeong-in/Google 드라이브/2021-1/알고리즘/hw2/main
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/subgraph_matching")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -16,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -34,5 +30,10 @@ endif()
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
