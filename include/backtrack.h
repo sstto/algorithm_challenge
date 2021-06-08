de89fca5e7cf69faa215745d62a3d00d89473626
stto <sspjyins@snu.ac.kr>
2021-06-08 12:53:23 +0900

    v2. add query node class

--- include/backtrack.h
+++ include/backtrack.h
@@ -28,4 +28,23 @@ private:
     std::vector<std::vector<Vertex>> nVisit;
 };
 
+class nodeQ{
+public:
+    nodeQ(Vertex v, std::vector<Vertex>* next = nullptr, std::vector<Vertex>* prev = nullptr);
+    ~nodeQ(){
+        delete next;delete prev;
+    }
+    std::vector<Vertex> *prev;
+    std::vector<Vertex> *next;
+
+    inline Vertex GetVertex();
+
+    inline void addNext(Vertex v);
+    inline void addPrev(Vertex v);
+private:
+    Vertex v;
+};
+inline Vertex nodeQ::GetVertex(){return v;}
+inline void nodeQ::addNext(Vertex v){next->push_back(v);}
+inline void nodeQ::addPrev(Vertex v){prev->push_back(v);}
 #endif  // BACKTRACK_H_
