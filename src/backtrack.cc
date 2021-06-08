5512eb4fc140ecd3c362f1781d0631408884df21
stto <sspjyins@snu.ac.kr>
2021-06-08 13:16:59 +0900

    v2. make Dag using linked list

--- src/backtrack.cc
+++ src/backtrack.cc
@@ -6,7 +6,16 @@
 #include "backtrack.h"
 #include <limits.h>
 #include <algorithm>
-//clock_t start, finish;
+#include <queue>
+
+// query tree
+nodeQ::nodeQ(Vertex v, std::vector<Vertex> *next, std::vector<Vertex> *prev)
+:v(v), next(next), prev(prev){}
+
+Vertex qRootVertex;
+std::vector<nodeQ*> q;
+std::vector<Vertex> q_sort;
+
 Backtrack::Backtrack() {
     count = 0;
 }
@@ -15,8 +24,57 @@ Backtrack::~Backtrack() {}
 void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
                                 const CandidateSet &cs) {
 
-//    start = clock();
+    //=============================debug===============================
+    clock_t start = clock();
     std::cout << "t " << query.GetNumVertices() << "\n";
+    //make a q
+    for (Vertex i = 0; i < query.GetNumVertices(); i++)
+    {
+        std::vector<Vertex>* prev = new std::vector<Vertex>();
+        std::vector<Vertex>* next = new std::vector<Vertex>();
+        q.push_back(new nodeQ(i, next, prev));
+    }
+
+
+    //find root of q
+    size_t factorCs = ULONG_MAX;size_t factorDegree = 1;
+    for (Vertex i = 0; i < query.GetNumVertices(); i++){
+        if(query.GetDegree(i) == 0)continue;
+        // argmin(cs/degree)
+        if (cs.GetCandidateSize(i)*factorDegree < factorCs*query.GetDegree(i)){
+            factorDegree =  factorCs*query.GetDegree(i);
+            factorCs = cs.GetCandidateSize(i);
+            qRootVertex = i;
+        }
+    }
+
+    // make dag Tree with q
+    std::queue<Vertex> quForBfs;
+    std::vector<bool> qVisited(query.GetNumVertices(), false);
+    std::vector<bool> visited(query.GetNumVertices(), false);
+    quForBfs.push(qRootVertex);
+    qVisited[qRootVertex] = true;
+
+    while(!q.empty()){
+        Vertex x = quForBfs.front();
+        quForBfs.pop();
+        visited[x] = true;
+        q_sort.push_back(x);
+
+        size_t startOffset = query.GetNeighborStartOffset(x);
+        size_t endOffset = query.GetNeighborEndOffset(x);
+        for(size_t i = startOffset; i< endOffset;i++){ //check i <= or <
+            size_t v_visit = query.GetNeighbor(i);
+            if (!visited[v_visit]){
+                q[x]->addNext(v_visit); q[v_visit]->addPrev(x);
+            }
+            if (!qVisited[v_visit]){
+                quForBfs.push(v_visit);
+                qVisited[v_visit] = true;
+            }
+        }
+    }
+
     nV.resize(query.GetNumVertices(), INT32_MAX);
     nVisit.resize(query.GetNumVertices());
     // find a root_vertex
